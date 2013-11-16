1206c4c3d714c3177b863f43f0b77b50cd14a12d
Alexander Ottenhoff <alexander.ottenhoff@gmail.com>
2013-11-16 14:25:38 +0100

    Added Runner tutorial + Disc Game player control demo

---
+++ Assets/Disc Arena/Disc.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Disc : MonoBehaviour {
+	
+	public float speed;
+	
+	// Use this for initialization
+	void Start () {
+			//rigidbody.AddExplosionForce(transform.forward * speed, ForceMode.VelocityChange, 1);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rigidbody.velocity = speed * rigidbody.velocity.normalized;
+	}
+}
