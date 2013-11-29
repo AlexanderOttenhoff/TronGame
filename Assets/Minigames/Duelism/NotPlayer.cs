65ac9cc32c226de207f9b6e09e43fda63e1be9f8
Alexander Ottenhoff <alexander.ottenhoff@gmail.com>
2013-11-29 20:59:49 +0100

    Updated Duelism, added models

---
+++ Assets/Minigames/Duelism/NotPlayer.cs
@@ -0,0 +1,134 @@
+﻿using UnityEngine;
+using System.Collections;
+
+
+[RequireComponent(typeof(CharacterController))]
+public class NotPlayer : MonoBehaviour {
+	
+	public float speed = 3.0f;
+	public float rotateSpeed = 3.0f;
+	
+	public float turnSmoothing = 15f;   // A smoothing value for turning the player.
+    public float speedDampTime = 0.1f;  // The damping for the speed parameter
+	
+	//public Disc projectile;
+	public float dashSpeed = 200;
+	private bool dash = false;
+	public float dashLength = 1;
+	private float dashStart = 0;
+	public float dashCD = 1;
+	private Vector3 movement;
+	
+	public GameObject hand;
+	public GameObject sword;
+	private float swingStart = 0;
+	private bool attack = false;
+	public float swingSpeed = 1000;
+	public float swingLength = 0.1f;
+	
+	// Use this for initialization
+	void Start () {
+		dashStart = 0;
+		movement = new Vector3(0,0,0);
+		//hand = GameObject.Find("Hand");
+		//sword = GameObject.Find("Hand/Sabre");
+		sword.SetActive(false);
+	}
+
+	// Update is called once per frame
+	void Update () {
+if(this.gameObject.name == "Duelist 1"){
+		CharacterController controller = GetComponent<CharacterController>();
+		//transform.Rotate (0, Input.GetAxis("Horizontal") * rotateSpeed, 0);	//Replace with ouya controller
+		//float curSpeed = speed * Input.GetAxis("Vertical");
+		
+		float h = Input.GetAxis("Horizontal");
+		//float h = SuperInputMapper.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_X);
+		float m = Input.GetAxis("Vertical");
+		//float m = SuperInputMapper.GetAxis(OuyaSDK.KeyEnum.AXIS_LSTICK_Y);
+		//float rightX = SuperInputMapper.GetAxis(OuyaSDK.KeyEnum.AXIS_RSTICK_X);
+		//float rightY = SuperInputMapper.GetAxis(OuyaSDK.KeyEnum.AXIS_RSTICK_Y);
+		float rx = Input.GetAxis ("Joy1 Axis 3");
+		float ry = Input.GetAxis ("Joy1 Axis 4");
+		float now = Time.time;
+
+		
+ 		if (Input.GetButtonDown("Jump")) {
+			Vector3 swing;
+			if(rx != 0f || ry != 0f) {
+				swing = new Vector3(rx, 0.0f, ry);
+				hand.transform.rotation = Quaternion.LookRotation(swing, Vector3.up);
+            		
+			} else {
+				swing = new Vector3(0.0f, 0.0f, 0.0f);
+				hand.transform.rotation = transform.localRotation;
+			}
+			swingStart = now;
+			sword.SetActive(true);
+        }
+		if(sword.activeSelf) {
+			if(now - swingStart > swingLength) {
+				sword.SetActive (false);
+			} else {
+				hand.transform.Rotate(Vector3.up * Time.deltaTime * swingSpeed);
+			}
+		}
+		
+		if (Input.GetButtonDown("Fire1")) {
+			if(dash == false && now - dashStart > dashCD) {
+				if(rx != 0f || ry != 0f) {
+					movement = new Vector3(rx, 0.0f, ry);
+            		transform.rotation = Quaternion.LookRotation(movement, Vector3.up);
+
+					dash = true;
+					dashStart = now;
+				}
+			}
+		} 
+		if(now - dashStart > dashLength)
+		{
+			dash = false;
+		}
+		if(dash) {
+			controller.Move(movement * dashSpeed * Time.deltaTime);
+		} else {
+			movement = new Vector3(h, 0.0f, m);
+			controller.Move(movement * speed * Time.deltaTime);
+			if(h != 0f || m != 0f)
+				rotating(h, m);
+        }
+	}
+}	
+	
+	void OnCollisionEnter(Collision collision) {
+		Debug.Log(this.ToString() + " collided with " + collision.gameObject.ToString() + " " + collision.relativeVelocity);
+		Debug.Log(collision.gameObject.name);
+		//if (collision.gameObject.tag == "Sword") {
+			//Destroy(collision.gameObject);
+			sword.SetActive(true);
+		//}
+	}
+
+//void OnTriggerEnter(Collider other) {
+//       Destroy(other.gameObject);
+//    }
+		
+	
+	    
+    void rotating (float horizontal, float vertical)
+    {
+        // Create a new vector of the horizontal and vertical inputs.
+        Vector3 targetDirection = new Vector3(horizontal, 0f, vertical);
+        
+        // Create a rotation based on this new vector assuming that up is the global y axis.
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection, Vector3.up);
+        
+        // Create a rotation that is an increment closer to the target rotation from the player's rotation.
+        Quaternion newRotation = Quaternion.Lerp(rigidbody.rotation, targetRotation, turnSmoothing * Time.deltaTime);
+        
+        // Change the players rotation to this new rotation.
+        rigidbody.MoveRotation(newRotation);
+    }
+
+}
+
--- Assets/Minigames/Player.cs
+++ Assets/Minigames/Player.cs
@@ -5,6 +5,11 @@ using System.Collections.Generic;
 public class Player : MonoBehaviour {
 
 	public float speed = 10f;
+	public float rotateSpeed = 3.0f;
+	
+	public float turnSmoothing = 15f;   // A smoothing value for turning the player.
+	public float speedDampTime = 0.1f;  // The damping for the speed parameter
+
 	public int ammunition = 3;
 	public bool dualAnalogMovement;
 	public bool isFacingWall = false;
@@ -12,29 +17,80 @@ public class Player : MonoBehaviour {
 	public OuyaPlayer playerNumber;
 
 	void Start() {
+		Reset();
+	}
+
+	public void Reset() {
 		if (GameController.currentGame != GameController.GameType.DiscArena) {
 			transform.FindChild("DiscShooter").GetComponent<DiscShooter>().enabled = false;
 		}
 	}
 
-	// Update is called once per frame
 	void Update () {
 		bool inShoot = OuyaInput.GetButtonDown(OuyaButton.LB, playerNumber) || OuyaInput.GetButtonDown(OuyaButton.RB, playerNumber);
 		Vector2 inLeft = new Vector2(OuyaInput.GetAxis(OuyaAxis.LX, playerNumber), OuyaInput.GetAxis(OuyaAxis.LY, playerNumber));
 		Vector2 inRight = new Vector2(OuyaInput.GetAxis(OuyaAxis.RX, playerNumber), OuyaInput.GetAxis(OuyaAxis.RY, playerNumber));
-
+		
 		if (dualAnalogMovement) {
 			float rotation = 0f;
 			if (inRight.magnitude > 0.25f) {
 				transform.rotation = Quaternion.LookRotation(new Vector3(inRight.x, 0, inRight.y));
 			}
 		} else {
-			if (inLeft != Vector2.zero) 
+			if (inLeft != Vector2.zero) {
 				transform.rotation = Quaternion.LookRotation(new Vector3(inLeft.x, 0, inLeft.y));
+			}
 		}
-
+		
 		Move (inLeft.x, inLeft.y);
 	}
+
+	//		float now = Time.time;
+	
+	//		if (inSword) {
+	//			Vector3 swing;
+	//			if(rx != 0f || ry != 0f) {
+	//				swing = new Vector3(rx, 0.0f, ry);
+	//				hand.transform.rotation = Quaternion.LookRotation(swing, Vector3.up);
+	//				
+	//			} else {
+	//				swing = new Vector3(0.0f, 0.0f, 0.0f);
+//				hand.transform.rotation = transform.localRotation;
+//			}
+//			swingStart = now;
+//			sword.SetActive(true);
+//		}
+//		if(sword.activeSelf) {
+//			if(now - swingStart > swingLength) {
+//				sword.SetActive (false);
+//			} else {
+//				hand.transform.Rotate(Vector3.up * Time.deltaTime * swingSpeed);
+//			}
+//		}
+		
+//		if (inDash) {
+//			if(dash == false && now - dashStart > dashCD) {
+//				if(rx != 0f || ry != 0f) {
+//					movement = new Vector3(rx, 0.0f, ry);
+//					transform.rotation = Quaternion.LookRotation(movement, Vector3.up);
+//					
+//					dash = true;
+//					dashStart = now;
+//				}
+//			}
+//		} 
+//		if(now - dashStart > dashLength)
+//		{
+//			dash = false;
+//		}
+//		if(dash) {
+//			controller.Move(movement * dashSpeed * Time.deltaTime);
+//		} else {
+//			movement = new Vector3(h, 0.0f, m);
+//			controller.Move(movement * speed * Time.deltaTime);
+//			if(h != 0f || m != 0f)
+//				rotating(h, m);
+//		}
 	
 	private void Move(float mvX, float mvY) {
 		CharacterController controller = GetComponent<CharacterController>();
