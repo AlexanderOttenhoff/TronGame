0cfc335ce05921485a689aedefc245ef1717833f
Lisa P <passinglisa@gmail.com>
2013-11-29 22:44:57 +0100

    monocycle rotation fun

---
+++ Assets/Minigames/KingOfTheHill/Scripts/RotateMonocycle.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotateMonocycle : MonoBehaviour {
+
+
+	public float rotationSpeed;
+	//GameObject monocycle = this.transform.parent;
+
+	void Start () {
+		this.rotationSpeed = transform.parent.transform.parent.transform.parent.rigidbody.velocity.magnitude;
+	}
+
+	void Update () {
+	
+		this.rotateWheel(this.rotationSpeed);
+		Debug.Log (this.rotationSpeed);
+		//Debug.Log(rigidbody.velocity);
+
+	}
+
+	void rotateWheel (float speed) {
+
+		transform.Rotate(Vector3.right * speed);
+
+	} 
+}
