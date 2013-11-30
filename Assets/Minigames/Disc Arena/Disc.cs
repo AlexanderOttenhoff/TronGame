7bd4e809fb1fd350dae6c16677ffe146a37df73c
braddrac <lepper@gmail.com>
2013-11-30 15:06:14 +0100

    functional duelism commit

--- Assets/Minigames/Disc Arena/Disc.cs
+++ Assets/Minigames/Disc Arena/Disc.cs
@@ -10,39 +10,39 @@ public class Disc : MonoBehaviour {
 	public void SetOwner(OuyaPlayer owner) {
 		playerOwner = owner;
 	}
-
+	
 	void Start() {
 		rigidbody.velocity = speed * transform.TransformDirection(Vector3.forward);
 	}
-
+	
 	void Update() {
 		//rigidbody.velocity = speed * transform.TransformDirection(Vector3.forward) * Time.deltaTime;
 		Debug.DrawRay(transform.position, rigidbody.velocity, Color.red);
-
+		
 		RaycastHit hit;
 		Ray ray = new Ray(transform.position, transform.forward);
 		if (Physics.Raycast(ray, out hit, speed * Time.deltaTime)) {
 			Debug.DrawLine(transform.position, hit.point, Color.red);
 			//Debug.DrawRay(hit.point, reflectVec, Color.green);
-
+			
 			if (hit.distance < speed * Time.deltaTime) {
 				print(hit.distance.ToString() + "/" + (speed * Time.deltaTime).ToString());
 			}
 			
 		}
 	}
-
+	
 	void OnCollisionEnter(Collision collision) {
-//		Debug.Log(this.ToString() + " collided with " + collision.gameObject.ToString() + " " + collision.relativeVelocity);
+		//		Debug.Log(this.ToString() + " collided with " + collision.gameObject.ToString() + " " + collision.relativeVelocity);
 		if (collision.gameObject.tag == "Player") {
 			collision.gameObject.SendMessage("HandleDiscCollision", this);
 		}
 	}
 	
-//	void OnCollisionExit(Collision collision) {
-//		if (collision.gameObject.tag == "Player") {
-//			hasLeftPlayer = true;
-//			Debug.Log(this.ToString() + " has left "  + collision.gameObject.ToString());
-//		}
-//	}
+	//	void OnCollisionExit(Collision collision) {
+	//		if (collision.gameObject.tag == "Player") {
+	//			hasLeftPlayer = true;
+	//			Debug.Log(this.ToString() + " has left "  + collision.gameObject.ToString());
+	//		}
+	//	}
 }
--- Assets/Minigames/Player.cs
+++ Assets/Minigames/Player.cs
@@ -1,53 +1,53 @@
 ﻿using UnityEngine;
 using System.Collections.Generic;
 public class Player : MonoBehaviour {
-
+	
 	public float speed = 10f;
 	public float rotateSpeed = 3.0f;
 	
 	public float turnSmoothing = 15f;   // A smoothing value for turning the player.
 	public float speedDampTime = 0.1f;  // The damping for the speed parameter
-
+	
 	public int ammunition = 3;
 	public bool dualAnalogMovement;
 	public bool isFacingWall = false;
-
+	
 	private Vector3 dashDirection;
 	private float dashStartTime;
 	public int dashSpeed = 200;
 	public float dashDuration = .25f;
 	public float dashCD = 1.0f;
 	private bool inDash = false;
-
+	
 	public Object ExplosionPrefab;
-
+	
 	CharacterController controller;
 	public OuyaPlayer playerNumber;
-
+	
 	void Start() {
 		Reset();
 		dashStartTime = Time.time;
 	}
-
+	
 	public void Reset() {
 		if (GameController.currentGame != GameController.GameType.DiscArena) {
-
+			
 		}
 	}
-
+	
 	void Update () {
 		float now = Time.time;
 		bool inShoot = OuyaInput.GetButtonDown(OuyaButton.LB, playerNumber) || OuyaInput.GetButtonDown(OuyaButton.RB, playerNumber);
 		Vector2 inLeft = new Vector2(OuyaInput.GetAxis(OuyaAxis.LX, playerNumber), OuyaInput.GetAxis(OuyaAxis.LY, playerNumber));
 		Vector2 inRight = new Vector2(OuyaInput.GetAxis(OuyaAxis.RX, playerNumber), OuyaInput.GetAxis(OuyaAxis.RY, playerNumber));
-
+		
 		if(OuyaInput.GetButtonDown(OuyaButton.LB, playerNumber)) {
 			if(inRight != Vector2.zero) 
 				playerDash(inRight, now);
 			else if(inLeft != Vector2.zero)
 				playerDash (inLeft, now);
 		}
-
+		
 		if(inDash) {
 			if(now - dashStartTime > dashDuration)
 			{
@@ -69,7 +69,7 @@ public class Player : MonoBehaviour {
 		}
 		rigidbody.MovePosition(new Vector3(rigidbody.position.x, 0, rigidbody.position.z));
 	}
-
+	
 	void objectRotate (float horizontal, float vertical)
 	{
 		// Create a new vector of the horizontal and vertical inputs.
@@ -84,29 +84,29 @@ public class Player : MonoBehaviour {
 		// Change the players rotation to this new rotation.
 		rigidbody.MoveRotation(newRotation);
 	}
-
+	
 	void playerDash(Vector2 dashDir, float now) {
 		if(inDash == false && now - dashStartTime > dashCD) {
 			dashDirection = new Vector3(dashDir.x, 0.0f, dashDir.y);
 			transform.rotation = Quaternion.LookRotation(dashDirection, Vector3.up);
-				
+			
 			inDash = true;
 			dashStartTime = now;
 		}
 	}
-
+	
 	private void Move(float mvX, float mvY) {
 		rigidbody.velocity = speed * new Vector3(mvX, 0, mvY);
 	}
-
+	
 	private void Die() {
 		//Initiate death animation
-        Debug.Log("I'm Dead :`(");
+		Debug.Log("I'm Dead :`(");
 		enabled = false;
 		Instantiate(ExplosionPrefab, transform.position, transform.rotation);
 		Destroy (this.gameObject);
 	}
-
+	
 	void OnControllerColliderHit(ControllerColliderHit hit){
 		if (GameController.currentGame == GameController.GameType.DiscArena) {
 			if (hit.gameObject.tag == "Disc") {
@@ -115,16 +115,16 @@ public class Player : MonoBehaviour {
 			}
 		}
 	}
-
+	
 	public void HandleDiscCollision(Disc disc) {
 		if (disc.playerOwner == playerNumber) { 
 			Destroy(disc.gameObject);
 			ammunition += 1;
 			isFacingWall = false;
 		} else {
-            Die();
+			Die();
 		}
 	}
-
+	
 }
 
