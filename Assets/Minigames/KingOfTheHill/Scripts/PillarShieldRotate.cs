ef5dd86000d8211e61f07decafeb98d47badd438
Lisa P <passinglisa@gmail.com>
2013-11-30 04:17:59 +0100

    and all the stuff that's missing and also not working

---
+++ Assets/Minigames/KingOfTheHill/Scripts/PillarShieldRotate.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PillarShieldRotate : MonoBehaviour {
+	
+
+	// Update is called once per frame
+	void Update () {
+	
+		transform.Find("geo_shild_1").transform.Rotate(Vector3.down * 5);
+		transform.Find("geo_shild_2").transform.Rotate(Vector3.up * 5);
+
+	}
+}
---
+++ Assets/Minigames/KingOfTheHill/Scripts/PlayerProperties.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerProperties : MonoBehaviour {
+
+	public string playerHook;
+
+	void Start () {
+
+		playerHook = name;
+		//Debug.Log(playerHook);
+	}
+
+	string getPlayerHook () {
+		return this.playerHook;
+	}
+}
